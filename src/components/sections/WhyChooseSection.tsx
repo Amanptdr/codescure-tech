2ef854696b65f7b436c903a050a8107192f1045c
amanpatidar <amanpatidar.dev@gmail.com>
2026-06-21 16:56:13 +0530

    issue fixed

--- src/components/sections/WhyChooseSection.tsx
+++ src/components/sections/WhyChooseSection.tsx
@@ -1,4 +1,5 @@
 import { SectionHeading } from '../ui/SectionHeading'
+import Vector from '../../assets/Vector.svg'
 import CallIcon from '../../assets/CallIcon.svg'
 import Calling1 from '../../assets/Calling1.svg'
 import Calling from '../../assets/Calling.svg'
@@ -15,28 +16,10 @@ export function WhyChooseSection() {
 
         <div className="split-grid split-grid--reverse">
           <div className="split-grid__visual split-grid__visual--freecall" aria-hidden="true">
-            <div className="orb orb--large"></div>
-            <article className="mini-card mini-card--floating mini-card--top">
-              <img src={CallIcon} alt="" />
-              <div>
-                <strong>Free Phone Screening</strong>
-                <p>Connect with new patients before the visit.</p>
-              </div>
-            </article>
-            <article className="mini-card mini-card--floating mini-card--right">
-              <img src={Calling} alt="" />
-              <div>
-                <strong>Instant voice triage</strong>
-                <p>Quick context, better recommendations.</p>
-              </div>
-            </article>
-            <article className="mini-card mini-card--floating mini-card--left">
-              <img src={Calling1} alt="" />
-              <div>
-                <strong>Warm introductions</strong>
-                <p>Feel confident before the first consult.</p>
-              </div>
-            </article>
+            <img className="split-grid__blob" src={Vector} alt="" />
+            <img className="split-grid__icon" src={CallIcon} alt="" />
+            <img className="split-grid__card split-grid__card--large" src={Calling} alt="" />
+            <img className="split-grid__card split-grid__card--small" src={Calling1} alt="" />
           </div>
 
           <div className="split-grid__content">
